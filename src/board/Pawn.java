9a81520c49e40257a37c992320e3b84890544d4a
DM126 <danny386126@gmail.com>
2021-05-20 15:23:47 -0400

    Add pawn movement

--- src/board/Pawn.java
+++ src/board/Pawn.java
@@ -19,7 +19,62 @@ public class Pawn extends Piece
 	@Override
 	public boolean canMove(Board board, int startRow, int startCol, int endRow, int endCol)
 	{
-		return true;
+		if (color == Color.BLACK)
+		{
+			if (board.isEmptySpace(endRow, endCol))
+			{
+				//Movement must be vertical if space is empty
+				if (startCol == endCol)
+				{
+					//pawns can move 1 space forward
+					if (endRow - startRow == 1)
+					{
+						return true;
+					}
+					else if (startRow == 1 && endRow == 3 && board.isEmptySpace(endRow, endCol))
+					{
+						//pawns can move 2 spaces if at the starting row and
+						//no piece is in between.
+						return true;
+					}
+				}
+			}
+			else if (Math.abs(startCol - endCol) == 1 && endRow - startRow == 1)
+			{
+				//space not empty, must be 1 space forward diagonally
+				return true;
+			}
+		}
+		else //white
+		{
+			if (board.isEmptySpace(endRow, endCol))
+			{
+				//movement must be vertical if the space is empty
+				if (startCol == endCol)
+				{
+					//pawns can move 1 space forward
+					if (startRow - endRow == 1)
+					{
+						return true;
+					}
+					else if (startRow == Board.BOARD_LENGTH - 2 && 
+							endRow == startRow - 2 && 
+							board.isEmptySpace(startRow - 1,  startCol))
+					{
+						//pawns can move 2 spaces if at the starting row and no
+						//piece is in between.
+						return true;
+					}
+				}
+			}
+			else if (Math.abs(startCol - endCol) == 1 && startRow - endRow == 1)
+			{
+				//space not empty, must be 1 space forward diagonally
+				return true;
+			}
+		}
+		
+		return false;
 	}
 	
 	@Override
