c8a2e20a775fa9675e518df8a157c0528cc9ffc0
DM126 <danny386126@gmail.com>
2021-05-20 16:26:08 -0400

    Add bishop movement

--- src/board/Bishop.java
+++ src/board/Bishop.java
@@ -19,7 +19,49 @@ public class Bishop extends Piece
 	@Override
 	public boolean canMove(Board board, int startRow, int startCol, int endRow, int endCol)
 	{
-		return true;
+		//can move diagonally
+		if (Math.abs(startRow - endRow) == Math.abs(startCol - endCol))
+		{
+			//check for pieces between start and end.
+			//only one of these loops will actually be entered, depending
+			//on which direction the bishop is moving.
+			for (int i = 1; startRow + i < endRow && startCol + i < endCol; i++)
+			{
+				//down-right
+				if (!board.isEmptySpace(startRow + i,  startCol + i))
+				{
+					return false;
+				}
+			}
+			for (int i = 1; startRow + i < endRow && startCol - i > endCol; i++)
+			{
+				//down-left
+				if (!board.isEmptySpace(startRow + i,  startCol - i))
+				{
+					return false;
+				}
+			}
+			for (int i = 1; startRow - i > endRow && startCol - i > endCol; i++)
+			{
+				//up-left
+				if (!board.isEmptySpace(startRow - i,  startCol - i))
+				{
+					return false;
+				}
+			}
+			for (int i = 1; startRow - i > endRow && startCol + i < endCol; i++)
+			{
+				//up-right
+				if (!board.isEmptySpace(startRow - i,  startCol + i))
+				{
+					return false;
+				}
+			}
+			
+			return true;
+		}
+		
+		return false;
 	}
 	
 	@Override
