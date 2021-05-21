98ee2482021557f0827ac49f4b9483bdb39f831c
DM126 <danny386126@gmail.com>
2021-05-21 11:11:10 -0400

    Add pawn promotion

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -66,6 +66,127 @@ public class GamePanel extends JPanel
 		return Math.abs(row - Board.BOARD_LENGTH) - 1;
 	}
 	
+	/**
+	 * Contains logic for when a space on the board is clicked.
+	 * If no piece is currently selected, selects a piece if one is clicked.
+	 * If a piece is currently selected, moves it or switches selections.
+	 * 
+	 * @param row the row clicked
+	 * @param col the column clicked
+	 */
+	private void spaceClicked(int row, int col)
+	{
+		if (selectedSpace == null)
+		{
+			Piece clicked = board.getPiece(row, col);
+			
+			if (clicked != null)
+			{
+				if (isWhitesTurn)
+				{
+					if (clicked.getColor() == Color.WHITE)
+					{
+						System.out.println("white selected " + clicked + " at " + row + "," + col);
+						selectedSpace = new Point(col, row);
+					}
+				}
+				else
+				{
+					if (clicked.getColor() == Color.BLACK)
+					{
+						System.out.println("black selected " + clicked + " at " + row + "," + col);
+						selectedSpace = new Point(col, row);
+					}
+				}
+			}
+		}
+		else //piece is already selected
+		{
+			//if a piece of the same color is selected, switch selection
+			if (board.getPiece(row, col) != null && 
+				board.getPiece(selectedSpace.y, selectedSpace.x).getColor() == board.getPiece(row, col).getColor())
+			{
+				System.out.println("switched selection to " + board.getPiece(row, col) + " at " + row + "," + col);
+				selectedSpace = new Point(col, row);
+			}
+			else //new selection is empty space or different color
+			{
+				//test if the piece can move
+				if (board.getPiece(selectedSpace.y, selectedSpace.x).canMove(board, selectedSpace.y, selectedSpace.x, row, col))
+				{
+					//move the piece and switch turns
+					board.movePiece(selectedSpace.y, selectedSpace.x, row, col);
+					System.out.println("moved " + board.getPiece(row, col) + " to " + row + "," + col);
+					
+					//check for pawn promotion
+					if (board.getPiece(row, col) instanceof Pawn)
+					{
+						promotePawn(row, col);
+					}
+					
+					isWhitesTurn = !isWhitesTurn;
+					
+					repaint();
+				}
+				else //TODO debug remove
+				{
+					System.out.println("could not move " + board.getPiece(selectedSpace.y, selectedSpace.x) + " to " + row + "," + col);
+				}
+				
+				selectedSpace = null;
+			}
+		}
+	}
+	
+	/**
+	 * checks if a pawn should be promoted, and promotes it if so
+	 * 
+	 * @param row the row of the pawn
+	 * @param column the column of the pawn
+	 */
+	private void promotePawn(int row, int column)
+	{
+		Piece pawn = board.getPiece(row, column);
+		if ((pawn.getColor() == Color.BLACK && row == Board.BOARD_LENGTH - 1) ||
+			(pawn.getColor() == Color.WHITE && row == 0))
+		{
+			String promoteTo = promotionPrompt();
+			Piece promoted;
+			if (promoteTo == "Bishop")
+			{
+				promoted = new Bishop(pawn.getColor());
+			}
+			else if (promoteTo == "Rook")
+			{
+				promoted = new Rook(pawn.getColor());
+			}
+			else
+			{
+				promoted = new Queen(pawn.getColor());
+			}
+			board.setPiece(promoted, row, column);
+		}
+	}
+	
+	/**
+	 * Opens a prompt for the user to select what to promote a pawn to
+	 * 
+	 * @return a string with the piece type to promote to (Bishop, Rook, or Queen)
+	 */
+	private String promotionPrompt()
+	{
+		String[] options = {"Bishop", "Rook", "Queen"};
+		int choice;
+		do
+		{
+			//TODO set icon to pawn image, depending on color
+			choice = JOptionPane.showOptionDialog(this, "Choose a piece to promote the pawn to", "Promote pawn", 
+						JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, "Queen");
+		}
+		while (choice == JOptionPane.CLOSED_OPTION);
+		return options[choice];
+	}
+	
 	private class ClickListener implements MouseListener
 	{
 		@Override
@@ -75,60 +196,7 @@ public class GamePanel extends JPanel
 			int r = e.getY() / SPACE_SIZE;
 			int c = e.getX() / SPACE_SIZE;
 			
-			if (selectedSpace == null)
-			{
-				Piece clicked = board.getPiece(r, c);
-				
-				if (clicked != null)
-				{
-					if (isWhitesTurn)
-					{
-						if (clicked.getColor() == Color.WHITE)
-						{
-							System.out.println("white selected " + clicked + " at " + r + "," + c);
-							selectedSpace = new Point(c, r);
-						}
-					}
-					else
-					{
-						if (clicked.getColor() == Color.BLACK)
-						{
-							System.out.println("black selected " + clicked + " at " + r + "," + c);
-							selectedSpace = new Point(c, r);
-						}
-					}
-				}
-			}
-			else //piece is already selected
-			{
-				//if a piece of the same color is selected, switch selection
-				if (board.getPiece(r, c) != null && 
-					board.getPiece(selectedSpace.y, selectedSpace.x).getColor() == board.getPiece(r, c).getColor())
-				{
-					System.out.println("switched selection to " + board.getPiece(r, c) + " at " + r + "," + c);
-					selectedSpace = new Point(c, r);
-				}
-				else //new selection is empty space or different color
-				{
-					//test if the piece can move
-					if (board.getPiece(selectedSpace.y, selectedSpace.x).canMove(board, selectedSpace.y, selectedSpace.x, r, c))
-					{
-						//move the piece and switch turns
-						board.movePiece(selectedSpace.y, selectedSpace.x, r, c);
-						System.out.println("moved " + board.getPiece(r, c) + " to " + r + "," + c);
-						
-						isWhitesTurn = !isWhitesTurn;
-						
-						repaint();
-					}
-					else //TODO debug remove
-					{
-						System.out.println("could not move to " + r + "," + c);
-					}
-					
-					selectedSpace = null;
-				}
-			}
+			spaceClicked(r, c);
 		}
 
 		//Unimplemented methods:
